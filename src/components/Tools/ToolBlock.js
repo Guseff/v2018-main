85a64ee6b08ec9fb9b355a6d9665d653db2a4faf
Guseff <vasily.guseff@gmail.com>
2019-04-19 16:25:43 +0300

    Tools with slide in Mobile Screen

--- src/components/Tools/ToolBlock.js
+++ src/components/Tools/ToolBlock.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 
 import './style.css';
 
-class ToolBlock extends Component { 
+class ToolBlock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { clsd: true };
+
+    this.closeHandle = this.closeHandle.bind(this);
+  }
+
+  closeHandle() {
+    this.setState(state => ({
+        clsd: !state.clsd 
+      }));
+  }
+
   render() {
     return (
-      <div className='tool-block'>
-        <div className='tool-block-img'>
+      <div className={classNames('tool-block', {'tool-block--closed': this.state.clsd})}>
+        <div className={classNames('tool-block-img', {'tool-el--closed': this.state.clsd})}>
           <img alt='' src={this.props.img} />
         </div>
         
-        <div className='tool-block-title'>
+        <button className={classNames('tool-block-title', {'tool-block-title--closed': this.state.clsd})} onClick={this.closeHandle}>
           {this.props.title}
-        </div>
+        </button>
 
-        <div className='tool-block-text'>
+        <div className={classNames('tool-block-text', {'tool-el--closed': this.state.clsd})}>
           {this.props.text}
         </div>
 
-        <button className='tool-btn'>Learn more</button>
-
+        <div className={classNames('tool-btn-wrap', {'tool-el--closed': this.state.clsd})}>
+          <button className={classNames('tool-btn')}>Learn more</button>
+        </div>
       </div>
     );
   }
