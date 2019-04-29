2cbfdda33822fdf65ea80f298a0f20eca2ee3005
Guseff <vasily.guseff@gmail.com>
2019-04-29 16:40:21 +0300

    Trasted done for wide screen

--- src/components/Trasted/index.js
+++ src/components/Trasted/index.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 
 import './style.css';
 
 const TRASTED = [
   {
-    title: 'KTI Group <br />Toivo Parnamaa',
+    title_1: 'Cointraffic',
+    title_2: 'Toivo Parnamaa',
     text: '“Bitsgap became my everyday tool to monitor what is happening on all crypto market in one place. Trading became so much easier now.“',
     img: require('../../assets/images/cointraffic.svg'),
     ico: require('../../assets/images/kti-small-logo.svg'),
   },
   {
-    title: 'KTI Group <br />Toivo Parnamaa',
+    title_1: 'Admiral Markets',
+    title_2: 'Toivo Parnamaa',
     text: '“Bitsgap became my everyday tool to monitor what is happening on all crypto market in one place. Trading became so much easier now.“',
     img: require('../../assets/images/admiral-markets.svg'),
     ico: require('../../assets/images/kti-small-logo.svg'),
   },
   {
-    title: 'KTI Group <br />Toivo Parnamaa',
+    title_1: 'EKL',
+    title_2: 'Toivo Parnamaa',
     text: '“Bitsgap became my everyday tool to monitor what is happening on all crypto market in one place. Trading became so much easier now.“',
     img: require('../../assets/images/ekl.svg'),
     ico: require('../../assets/images/kti-small-logo.svg'),
   },
   {
-    title: 'KTI Group <br />Toivo Parnamaa',
+    title_1: 'KTI Group',
+    title_2: 'Toivo Parnamaa',
     text: '“Bitsgap became my everyday tool to monitor what is happening on all crypto market in one place. Trading became so much easier now.“',
     img: require('../../assets/images/kti.svg'),
     ico: require('../../assets/images/kti-small-logo.svg'),
@@ -39,9 +44,9 @@ class Trasted extends Component {
     this.changeAct = this.changeAct.bind(this);
   }
 
-  changeAct(num) {
-    this.setState(state => ({
-      act: this.state.act === 3 ? 0 : this.state.act + 1,
+  changeAct(i) {
+    this.setState(({
+      act: i,
     }))
   }
 
@@ -55,23 +60,27 @@ class Trasted extends Component {
 
           <div className='trasted-info'>
             <div className='trasted-info-text'>
-              “Bitsgap became my everyday tool to monitor 
-              what is happening on all crypto market in one 
-              place. Trading became so much easier now.“
+              {TRASTED[this.state.act].text}
             </div>
             <div className='trasted-info-title'>
               <div className='trasted-info-ico'>
-                <img alt='' src={TRASTED[1].ico} />
+                <img alt='' src={TRASTED[this.state.act].ico} />
               </div>
-              KTI Group <br />Toivo Parnamaa
+              {TRASTED[this.state.act].title_1}<br />
+              {TRASTED[this.state.act].title_2}
+            </div>
+            <div className='trasted-info-btn-wrap'>
+              {TRASTED.map((elem, i) => (
+                <button key={i} onClick={() => this.changeAct(i)} className={classNames('trasted-info-btn', {'trasted-info-btn--active': this.state.act === i})}></button>
+              ))}
             </div>
           </div>
 
           <div className='trasted-list'>
             {TRASTED.map((elem, i) => (
-              <div key={i} className='trasted-list-element'>
-                <img alt='' src={elem.img} onClick={this.changeAct} />
-              </div>
+              <button key={i} onClick={() => this.changeAct(i)} className='trasted-list-element'>
+                <img alt='' src={elem.img} />
+              </button>
             ))}
           </div>  
         </div>        
