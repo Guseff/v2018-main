930cfd1c86c0580eb985267f92dbe2a9f170435c
Guseff <vasily.guseff@gmail.com>
2019-05-07 09:51:34 +0300

    package.json

--- src/components/Footer/InfoList.js
+++ src/components/Footer/InfoList.js
@@ -4,7 +4,6 @@ const InfoList = props => (
   <ul className='footer-info-list'>
     {props.list.map((item, i) => (
       <li key={i}>
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
         <a href={item.ref}>{item.text}</a>
       </li>
     ))}
